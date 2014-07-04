b679595fd134344ec843ad1d0b41574cae1ecfce
Ayysir <dresadd09691@gmail.com>
2014-07-04 14:29:37 -0400

    Use Prebuilt sensors

--- find7/find7-vendor-blobs.mk
+++ find7/find7-vendor-blobs.mk
@@ -54,6 +54,7 @@ PRODUCT_COPY_FILES += \
     vendor/oppo/find7/proprietary/etc/firmware/cpp_firmware_v1_2_0.fw:system/etc/firmware/cpp_firmware_v1_2_0.fw \
     vendor/oppo/find7/proprietary/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
     vendor/oppo/find7/proprietary/vendor/lib/libAKM8963.so:system/vendor/lib/libAKM8963.so \
+    vendor/oppo/find7/proprietary/lib/hw/sensors.msm8974.so:system/lib/hw/sensors.msm8974.so \
     vendor/oppo/find7/proprietary/etc/firmware/audience-es325-fw.bin:system/etc/firmware/audience-es325-fw.bin
 
 PRODUCT_COPY_FILES += \
